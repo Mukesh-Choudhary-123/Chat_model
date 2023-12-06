100f24dfcfe13b5d56a8c7657f0ea5b636058c0b
Mukesh Choudhary <mukeshchoudhary1062003@gmail.com>
2023-12-06 09:30:47 +0530

    logic of Routes

---
+++ index.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const path = require("path");
+const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+main()
+  .then(() => {
+    console.log("Connetion Done !");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  //   console.log(chats);
+  res.render("index.ejs", { chats });
+});
+
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    create_at: new Date(),
+  });
+  newChat
+    .save()
+    .then((res) => {
+      console.log(newChat);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
+});
+
+//Edit Route
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+//Update Route
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body;
+  let updateChat = await Chat.findByIdAndUpdate(
+    id,
+    { msg: newMsg },
+    { runValidators: true, new: true }
+  );
+  console.log(updateChat);
+  res.redirect("/chats");
+});
+
+//Destroy Route
+app.delete("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  console.log(deletedChat);
+  res.redirect("/chats");
+});
+
+app.get("/", (req, res) => {
+  res.send("Root is Working");
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
